956ff60be782c39fd6d0eb5f8bd953d9f711bd29
juice <juice.yoon@goodoc.co.kr>
2025-03-30 17:01:02 +0900

    set: 온다움 color 값

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,18 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    colors:{
+      main:'#F57C00',
+      second:'#F8A047',
+      third:'#FFE8BF',
+      gray_1:'#EDEDED',
+      gray_2:'#F6F6F6',
+      font_color_1:'#505050',
+      font_color_2:'#AEAEAE',
+      white:'#FAFAFA'
+    },
     extend: {},
   },
+
   plugins: [],
 };

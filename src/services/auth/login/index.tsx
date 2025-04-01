7172188636c4e6be9717de62d7b790f43cff4576
Jiwon Jeong <jjw04033@konkuk.ac.kr>
2025-04-01 16:01:43 +0900

    feat: add login layout

--- src/services/auth/login/index.tsx
+++ src/services/auth/login/index.tsx
@@ -1,5 +1,7 @@
 import { useNavigate, useLocation } from "react-router-dom";
 import { useAuthStore } from "../../../store/auth";
+import LoginButton from "../../../commons/inputs/Button/login";
+import GlobalNavigation from "../../../commons/navigation/GlobalNavigation";
 
 // 실제 구글 OAuth 로그인은 예시로 대체 (Google API 통합은 별도)
 function LoginPage() {
@@ -21,13 +23,27 @@ function LoginPage() {
   };
 
   return (
-    <div className="p-4 text-center">
-      <button
-        onClick={handleGoogleLogin}
-        className="bg-blue-500 text-white px-4 py-2 rounded"
-      >
-        Sign in with Google
-      </button>
+    <div>
+      <GlobalNavigation />
+      <main className="pb-44">
+        <h2 className="text-lg text-gray-5 my-4 font-normal pb-[20px]">
+          Hello
+        </h2>
+        <p className="text-[40px] font-bold font-['Figtree'] leading-[56px] pb-[32px]">
+          Is this
+          <br />
+          your first time using
+          <br />
+          <span className="text-orange-2">OnDaum?</span>
+        </p>
+        <p className="text-font-color text-[22px] font-normal font-['Pretendard'] leading-[30.80px]">
+          {"You can sign up in just" + " "}
+          <span className="text-orange-2">one minute.</span>
+        </p>
+        <div className="fixed w-screen bottom-16 py-4 px-4 text-center">
+          <LoginButton onClick={handleGoogleLogin} />
+        </div>
+      </main>
     </div>
   );
 }

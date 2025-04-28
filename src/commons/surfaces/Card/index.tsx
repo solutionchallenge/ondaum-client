13b7b4ea5610195bf2d8547dda561677f84bc8e2
Jiwon Jeong <jjw04033@konkuk.ac.kr>
2025-04-29 01:57:24 +0900

    feat: add Card component

---
+++ src/commons/surfaces/Card/index.tsx
@@ -0,0 +1,21 @@
+﻿interface CardProps {
+  onClick: () => void;
+  title?: string;
+  description?: string;
+}
+
+export default function Card({ onClick, title, description }: CardProps) {
+  return (
+    <div
+      onClick={onClick}
+      className="w-full mt-5 max-w-sm px-5 py-4 bg-orange-1 rounded-xl outline outline-1 outline-[#f8a047] flex flex-col gap-2 cursor-pointer"
+    >
+      <div className="w-full flex flex-col gap-2">
+        <div className="text-font-color text-xl font-bold leading-7">
+          {title}
+        </div>
+        <div className="text-font-color text-sm font-normal">{description}</div>
+      </div>
+    </div>
+  );
+}
--- src/services/home/index.tsx
+++ src/services/home/index.tsx
@@ -1,4 +1,14 @@
+import Card from "../../commons/surfaces/Card";
+
 function HomePage() {
-  return <>home</>;
+  return (
+    <main className="flex justify-center items-center h-screen pb-44">
+      <Card
+        onClick={() => {}}
+        title="Let's chat with Umi!"
+        description="Feel free to share your worries with Umi!"
+      />
+    </main>
+  );
 }
 export default HomePage;

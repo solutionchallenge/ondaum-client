e3671ffa13beb5b97020d86005af52a71af4d013
Jiwon Jeong <jjw04033@konkuk.ac.kr>
2025-04-29 02:32:27 +0900

    feat: add avatar

--- src/services/home/index.tsx
+++ src/services/home/index.tsx
@@ -1,3 +1,4 @@
+import UmAvatar from "../../commons/data-display/Avatar";
 import DateChip from "../../commons/data-display/Chip";
 import Card from "../../commons/surfaces/Card";
 
@@ -10,6 +11,14 @@ function HomePage() {
         description="Feel free to share your worries with Umi!"
       />
       <DateChip date={new Date("2025-03-09T16:55:00")} />
+      <div className="flex flex-row gap-2">
+        <UmAvatar />
+        <div className="flex flex-col">
+          <div className="justify-start text-main font-semibold font-['Pretendard']">
+            Um
+          </div>
+        </div>
+      </div>
     </main>
   );
 }

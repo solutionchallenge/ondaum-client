5d5f7abcd45bc3a7ca0efc8033df261351172363
Jiwon Jeong <jjw04033@konkuk.ac.kr>
2025-04-29 02:22:08 +0900

    feat: add datechip

--- src/services/home/index.tsx
+++ src/services/home/index.tsx
@@ -1,13 +1,15 @@
+import DateChip from "../../commons/data-display/Chip";
 import Card from "../../commons/surfaces/Card";
 
 function HomePage() {
   return (
-    <main className="flex justify-center items-center h-screen pb-44">
+    <main className="flex flex-col justify-center items-center h-screen pb-44">
       <Card
         onClick={() => {}}
         title="Let's chat with Umi!"
         description="Feel free to share your worries with Umi!"
       />
+      <DateChip date={new Date("2025-03-09T16:55:00")} />
     </main>
   );
 }

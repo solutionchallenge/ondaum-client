935224db81c1639e2de364e329f93bd835e86f3b
Jiwon Jeong <jjw04033@konkuk.ac.kr>
2025-04-29 18:53:02 +0900

    design: fix chat layout

--- src/commons/data-display/List/index.tsx
+++ src/commons/data-display/List/index.tsx
@@ -4,7 +4,7 @@
 
 export default function ListItem({ contents }: ListItemProps) {
   return (
-    <div className="px-4 py-2 max-w-[78%] bg-gray-2 rounded-tl-[15px] rounded-tr-[15px] rounded-br-[15px] outline outline-1 outline-offset-[-0.5px] outline-gray-1 inline-flex gap-3 overflow-hidden">
+    <div className="px-4 py-2 max-w-[80%] bg-gray-2 rounded-tl-[15px] rounded-tr-[15px] rounded-br-[15px] outline outline-1 outline-offset-[-0.5px] outline-gray-1 inline-flex gap-3 overflow-hidden">
       <div className="justify-start text-font-color text-sm font-['Pretendard'] leading-tight">
         {contents.map((item, index) => (
           <span key={index} className={item.bold ? "font-bold" : "font-normal"}>

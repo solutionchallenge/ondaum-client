97f99e66d5c644667e533c5eea63744b74115735
Jiwon Jeong <jjw04033@konkuk.ac.kr>
2025-05-04 00:49:10 +0900

    feat: add stepper component

---
+++ src/commons/navigation/Stepper/index.tsx
@@ -0,0 +1,20 @@
+interface StepperProps {
+  currentStep: number; // 1-based index
+}
+
+const Stepper = ({ currentStep }: StepperProps) => {
+  return (
+    <div className="w-full flex justify-start items-center gap-2">
+      {[1, 2, 3].map((step) => (
+        <div
+          key={step}
+          className={`flex-1 h-2.5 rounded-[20px] ${
+            step === currentStep ? "bg-third" : "bg-[#d9d9d9]"
+          }`}
+        />
+      ))}
+    </div>
+  );
+};
+
+export default Stepper;

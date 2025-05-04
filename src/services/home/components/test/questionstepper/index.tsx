9740b6ddb9d2f0d3e32d83f4ab8bb6caf0801476
Jiwon Jeong <jjw04033@konkuk.ac.kr>
2025-05-05 03:47:35 +0900

    feat: add progress bar component

---
+++ src/services/home/components/test/questionstepper/index.tsx
@@ -0,0 +1,25 @@
+﻿interface QuestionStepperProps {
+  currentStep: number; // 1-based index
+  totalSteps: number;
+}
+
+const QuestionStepper = ({ currentStep, totalSteps }: QuestionStepperProps) => {
+  return (
+    <div className="w-full p-3 flex justify-start items-center gap-2">
+      {Array.from({ length: totalSteps }, (_, i) => i + 1).map((step) => (
+        <div
+          key={step}
+          className={`flex-1 h-2 rounded-[20px] ${
+            step === currentStep
+              ? "bg-third"
+              : step < currentStep
+                ? "bg-main"
+                : "bg-third"
+          }`}
+        />
+      ))}
+    </div>
+  );
+};
+
+export default QuestionStepper;

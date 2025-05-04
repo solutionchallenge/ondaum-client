7da28ae14abf498319f765a596cc92a207bf71b9
Jiwon Jeong <jjw04033@konkuk.ac.kr>
2025-05-05 01:58:46 +0900

    fix: fix test toggle timing

--- src/services/home/components/testsection/index.tsx
+++ src/services/home/components/testsection/index.tsx
@@ -1,4 +1,5 @@
-﻿import UmAvatar from "../../../../commons/data-display/Avatar";
+﻿import { useState } from "react";
+import UmAvatar from "../../../../commons/data-display/Avatar";
 import TestChatList from "../../../../commons/data-display/List/testgroup";
 import TestToggle from "../../../../commons/inputs/ToggleButton/test";
 
@@ -13,14 +14,22 @@ const TestSection = ({
   setSelectedTest,
   setIsChatFinished,
 }: Props) => {
+  const [isListFinished, setIsListFinished] = useState(false);
   return (
     <div className="w-full flex flex-row justify-center gap-2 ml-3">
       <UmAvatar />
       <div className="flex flex-col w-full justify-start">
         <div className="text-main font-semibold font-['Pretendard']">Um</div>
         <div className="overflow-x-auto pr-2">
-          <TestChatList onFinish={() => setIsChatFinished(true)} />
-          <TestToggle selected={selectedTest} onSelect={setSelectedTest} />
+          <TestChatList
+            onFinish={() => {
+              setIsChatFinished(true);
+              setIsListFinished(true);
+            }}
+          />
+          {isListFinished && (
+            <TestToggle selected={selectedTest} onSelect={setSelectedTest} />
+          )}
         </div>
       </div>
     </div>

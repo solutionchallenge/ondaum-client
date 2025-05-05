afd1ce072af5ef789254b57033c5cac33e5d5414
Jiwon Jeong <jjw04033@konkuk.ac.kr>
2025-05-06 03:45:20 +0900

    feat: add policy component

---
+++ src/services/setting/component/policy/index.tsx
@@ -0,0 +1,33 @@
+﻿const PolicySection = () => {
+  return (
+    <div className="w-full max-w-md px-5 py-4 bg-white rounded-xl border border-main flex flex-col gap-4">
+      <div className="text-lg font-medium text-font-color">
+        Terms and Policies
+      </div>
+
+      <div className="bg-third rounded-xl border border-main px-5 py-3">
+        <div className="flex flex-col gap-1">
+          <div className="text-base font-semibold text-font-color">
+            Terms of Service
+          </div>
+          <div className="text-sm text-font-color2">Ver 2.1.0</div>
+        </div>
+      </div>
+
+      <div className="bg-third rounded-xl border border-main px-5 py-3">
+        <div className="flex flex-col gap-1">
+          <div className="text-base font-semibold text-font-color">
+            Privacy Policy
+          </div>
+          <div className="text-sm text-font-color2">Ver 2.1.0</div>
+        </div>
+      </div>
+
+      <div className="text-sm text-font-color">
+        Last updated: February 15, 2024
+      </div>
+    </div>
+  );
+};
+
+export default PolicySection;
--- src/services/setting/index.tsx
+++ src/services/setting/index.tsx
@@ -1,12 +1,14 @@
 ﻿import BottomNavigation from "../../commons/navigation/BottomNavigation";
 import GlobalNavigation from "../../commons/navigation/GlobalNavigation";
 import LanguageSetting from "./component/languagesetting";
+import PolicySection from "./component/policy";
 
 function SettingPage() {
   return (
-    <main className="relative pt-24 px-3 flex flex-col h-screen overflow-hidden bg-white">
+    <main className="relative pt-24 px-3 gap-5 flex flex-col h-screen overflow-hidden bg-white">
       <GlobalNavigation />
       <LanguageSetting />
+      <PolicySection />
       <BottomNavigation />
     </main>
   );

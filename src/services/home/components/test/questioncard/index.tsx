3d3036bebbf84602483fefab9c39dde102737d19
Jiwon Jeong <jjw04033@konkuk.ac.kr>
2025-05-05 21:49:22 +0900

    feat: add questioncard
    - questionstepper 하위 컴포넌트 추가

---
+++ src/services/home/components/test/questioncard/index.tsx
@@ -0,0 +1,25 @@
+﻿import QuestionStepperText from "../questionsteppertext";
+import React from "react";
+
+interface QuestionCardProps {
+  questionText: string | React.ReactNode;
+  currentStep: number;
+  totalSteps: number;
+}
+
+const QuestionCard = ({
+  questionText,
+  currentStep,
+  totalSteps,
+}: QuestionCardProps) => {
+  return (
+    <div className="w-full px-4 py-5 bg-white flex flex-col gap-6 sm:gap-3">
+      <QuestionStepperText currentStep={currentStep} totalSteps={totalSteps} />
+      <div className="text-font-color text-xl sm:text-2xl md:text-[28px] font-semibold leading-snug">
+        {questionText}
+      </div>
+    </div>
+  );
+};
+
+export default QuestionCard;
---
+++ src/services/home/components/test/questionsteppertext/index.tsx
@@ -0,0 +1,22 @@
+﻿interface QuestionStepperProps {
+  currentStep: number; // 1-based index
+  totalSteps: number;
+}
+
+const QuestionStepperText = ({
+  currentStep,
+  totalSteps,
+}: QuestionStepperProps) => {
+  return (
+    <div className="inline-flex items-end gap-1 sm:gap-2">
+      <div className="text-second text-2xl sm:text-3xl md:text-4xl font-semibold leading-none">
+        {String(currentStep).padStart(2, "0")}
+      </div>
+      <div className="text-font-color2 text-lg sm:text-xl md:text-2xl font-normal leading-none">
+        / {String(totalSteps).padStart(2, "0")}
+      </div>
+    </div>
+  );
+};
+
+export default QuestionStepperText;

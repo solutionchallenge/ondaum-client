156f1101ef52d6fb6fcf552c1e836d61346936d2
juice <juice.yoon@goodoc.co.kr>
2025-05-07 22:49:42 +0900

    add: checkbox 컴포넌트 추가

--- src/commons/inputs/CheckBox/index.tsx
+++ src/commons/inputs/CheckBox/index.tsx
@@ -1,32 +1,32 @@
 import { useEffect, useState } from "react";
 
-interface CheckBoxProps{
-    defaultChecked:boolean;
-    label:string,
-    onChange:(key:string, value:boolean)=>void;
+interface CheckBoxProps {
+  defaultChecked: boolean;
+  label: string;
+  onChange: (key: string, value: boolean) => void;
 }
 
-function CheckBox({label, onChange, defaultChecked}:CheckBoxProps){
-    const [checked, setChecked] = useState<boolean>();
-    
-    useEffect(()=>{
-        setChecked(defaultChecked);
-    },[defaultChecked]);
-    
-    const check = (key:string, value:boolean)=>{
-        setChecked(value);
-        onChange(key, value);
-    }
-    return(
-        <label className="flex items-center space-x-2 cursor-pointer">
-                <input
-                    type="checkbox"
-                    checked={checked}
-                    onChange={(e) => check(label, e.target.checked)}
-                    className="accent-second w-4 h-4 bg-gray-100 border-font-color text-white"
-                />
-            <span className="text-gray-700">{label}</span>
-        </label>
-    )
-}       
+function CheckBox({ label, onChange, defaultChecked }: CheckBoxProps) {
+  const [checked, setChecked] = useState<boolean>();
+
+  useEffect(() => {
+    setChecked(defaultChecked);
+  }, [defaultChecked]);
+
+  const check = (key: string, value: boolean) => {
+    setChecked(value);
+    onChange(key, value);
+  };
+  return (
+    <label className="flex items-center space-x-2 cursor-pointer">
+      <input
+        type="checkbox"
+        checked={checked}
+        onChange={(e) => check(label, e.target.checked)}
+        className="accent-second w-4 h-4  bg-white text-white"
+      />
+      <span className="text-font-color">{label}</span>
+    </label>
+  );
+}
 export default CheckBox;

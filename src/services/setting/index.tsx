a198e3e7abec46e3ac245098b5e515c2ef40ced4
juice <juice.yoon@goodoc.co.kr>
2025-05-17 01:25:46 +0900

    add: navigate login page

--- src/services/setting/index.tsx
+++ src/services/setting/index.tsx
@@ -4,9 +4,11 @@ import LanguageSetting from "./LanguageSetting";
 import PolicySection from "./Policy";
 import FAQSection from "./Question";
 import { useAuthStore } from "../../store/auth";
+import { useNavigate } from "react-router-dom";
 
 function SettingPage() {
   const { logout } = useAuthStore();
+  const navigate = useNavigate();
   return (
     <main className="pb-52 pt-16 mt-5 bg-white">
       <div className="flex flex-col items-center gap-5 w-full px-5">
@@ -18,12 +20,12 @@ function SettingPage() {
           className=" text-main font-bold  underline cursor-pointer"
           onClick={() => {
             logout();
+            navigate("/login");
           }}
         >
           Logout
         </p>
       </div>
-
       <BottomNavigation />
     </main>
   );
